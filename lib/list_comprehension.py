daba4451b814909efe567c3b5f859ebe4067bd31
Ryan Murzyn <orian3737@gmail.com>
2024-06-26 17:48:27 -0400

    pass

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
 
-def make_exclamation(sentence_list):
-    pass
+def return_evens(numbers):
+    return [num for num in numbers if num % 2 == 0]
+
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
+
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+print(make_exclamation([ "I like computers!","I require coffee!","Live long and prosper!"]))
